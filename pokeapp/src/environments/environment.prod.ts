6977affedc47e07d8e5af3569c044fc527e499f1
Igor Augusto <igor-augustofc@hotmail.com>
2025-06-28 14:39:39 -0300

    Corringindo erro que estava impedindo rodar o comando para o projeto ir para a produção

--- pokeapp/src/environments/environment.prod.ts
+++ pokeapp/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA7jQdgGpKox91DUl7556vBhOHyOmfUnro',
+    authDomain: 'login-pokeapi-7a5ad.firebaseapp.com',
+    projectId: 'login-pokeapi-7a5ad',
+    storageBucket: 'login-pokeapi-7a5ad.appspot.com',
+    messagingSenderId: '588037905459',
+    appId: '1:588037905459:web:bd1a2e788021fe23f45746',
+  },
 };
